7bdbb7a8c1fdcc7cc325eefcb4743a81429fdf8f
Hye <hye@hpcnt.com>
2021-01-15 00:32:17 +0900

    add: PageFooter component

---
+++ src/components/PageFooter/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles({
+  Container: {
+    position: 'fixed',
+    bottom: 0,
+    marginBottom: 24,
+  },
+});
+
+const PageFooter: React.FC = ({ children }) => {
+  const classes = useStyle();
+
+  return (
+    <Grid container direction='row' justify='space-between' alignItems='center' className={classes.Container}>
+      { children }
+    </Grid>
+  );
+};
+
+export default PageFooter;
