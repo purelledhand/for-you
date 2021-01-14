998d317678aef48df3c774193988e97f01e50fbf
Hye <hye@hpcnt.com>
2021-01-15 03:21:51 +0900

    add: cspHtmlWebpackPlugin

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+const { override } = require('customize-cra');
+const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
+
+const cspConfigPolicy = {
+    'base-uri': "'self'",
+    'object-src': "'none'",
+    'script-src': ["'self'", "https://www.googletagmanager.com"],
+    'style-src': ["'self'"]
+};
+
+function addCspHtmlWebpackPlugin(config) {
+    if(process.env.NODE_ENV === 'production') {
+        config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy));
+    }
+
+    return config;
+}
+
+module.exports = {
+    webpack: override(addCspHtmlWebpackPlugin),
+};
